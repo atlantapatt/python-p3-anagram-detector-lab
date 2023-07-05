2e9629e828359fff1ea961259d1631686712a997
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-07-05 13:19:31 -0700

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        word = self.word
+        match = []
+        for anagram in anagrams:
+            if(sorted(word) == sorted(anagram)):
+                match.append(anagram)
+        return match
